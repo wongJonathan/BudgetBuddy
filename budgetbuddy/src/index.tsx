e42d5a16acf6820dfc04a145467b9739305f410b
jonahan <wong.jonathan@live.com>
2019-10-26 18:47:37 -0400

    Added expense page that holds all the different types of expenses.

--- budgetbuddy/src/index.tsx
+++ budgetbuddy/src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ExpenseForms from './components/expenses_page/expenseForm';
+import ExpensePage from './components/expenses_page/expensePage';
 
-ReactDOM.render(<ExpenseForms />, document.getElementById('root'));
+ReactDOM.render(<ExpensePage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
